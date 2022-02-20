8a78ee537048ad939fa79330d1aef1bf2176d9e8
Roman Leontev <roman.l.v@yandex.ru>
2022-02-20 15:27:28 +0300

    Add h2

--- main.py
+++ main.py
@@ -9,11 +9,13 @@ class Parser():
     default_p = ''
     default_h1 = ''
     default_h2 = ''
+    default_h3 = ''
 
-    def __init__(self, p=default_p, h1=default_h1, h2=default_h2) :
+    def __init__(self, p=default_p, h1=default_h1, h2=default_h2, h3=default_h3):
         self.p = p
         self.h1 = h1
         self.h2 = h2
+        self.h3 = h3
 
     def find_h1(self, html_h1):
         self.h1 = html_h1.findAll('h1')
@@ -28,6 +30,12 @@ class Parser():
         for h2 in self.h2:
             print(h2.text)
 
+    def find_h3(self, html_h3):
+        self.h3 = html_h3.findAll('h3')
+        print(f'Заголовки h3')
+        for h3 in self.h3:
+            print(h3.text)
+
 
     def find_p(self, html_p):
         self.p = html_p.findAll('p')
@@ -40,7 +48,11 @@ if __name__ == '__main__':
     parser = Parser()
 
     parser.find_h1(html_soup)
+    print()
     parser.find_h2(html_soup)
+    print()
+    parser.find_h3(html_soup)
+    print()
     parser.find_p(html_soup)
 
 
