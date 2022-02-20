dcb742f880515d3dc3a0a2fcc08b9fabcd27bed0
Roman Leontev <roman.l.v@yandex.ru>
2022-02-20 15:21:05 +0300

    Add h2

--- main.py
+++ main.py
@@ -8,28 +8,39 @@ html_soup = bs(adress.text, 'lxml')
 class Parser():
     default_p = ''
     default_h1 = ''
+    default_h2 = ''
 
-    def __init__(self, p=default_p, h1=default_h1) :
+    def __init__(self, p=default_p, h1=default_h1, h2=default_h2) :
         self.p = p
         self.h1 = h1
-
-    def find_p(self, html_p):
-        self.p = html_p.findAll('p')
-        for p in self.p:
-            print(p.text)
+        self.h2 = h2
 
     def find_h1(self, html_h1):
         self.h1 = html_h1.findAll('h1')
+        print(f'Заголовки h1')
         for h1 in self.h1:
             print(h1.text)
 
 
+    def find_h2(self, html_h2):
+        self.h2 = html_h2.findAll('h2')
+        print(f'Заголовки h2')
+        for h2 in self.h2:
+            print(h2.text)
+
+
+    def find_p(self, html_p):
+        self.p = html_p.findAll('p')
+        print(f'Абзацы p')
+        for p in self.p:
+            print(p.text, sep='n/')
+
 if __name__ == '__main__':
 
     parser = Parser()
-    #parser.find_p(html_soup)
+
     parser.find_h1(html_soup)
-
-
+    parser.find_h2(html_soup)
+    parser.find_p(html_soup)
 
 
